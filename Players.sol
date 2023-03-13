f1eb15b20a7fdc4590fb69852ffdf4b951754491
rg3614 <rg3614@rit.edu>
2023-03-12 23:19:10 -0400

    questing, player nft

--- Players.sol
+++ Players.sol
@@ -3,12 +3,13 @@ pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts@4.8.2/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts@4.8.2/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts@4.8.2/token/ERC721/utils/ERC721Holder.sol";
 import "@openzeppelin/contracts@4.8.2/access/Ownable.sol";
 import "@openzeppelin/contracts@4.8.2/utils/Counters.sol";
 
 import "./Items.sol";
 
-contract Players is ERC721, ERC721Burnable, Ownable {
+contract Players is ERC721, ERC721Burnable, Ownable, ERC721Holder {
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
@@ -42,6 +43,15 @@ contract Players is ERC721, ERC721Burnable, Ownable {
         uint256 HitpointsXpForLevel;
     }
 
+    struct PlayerQuesting {
+        bool isQuesting;
+        uint256 time;
+        uint256 typeQuest;
+        address owner;
+    }
+
+    mapping(uint256 => PlayerQuesting) public playerQuesting;
+
     Player[] players;
 
     modifier onlyOwnerOf(uint256 _playerId) {
@@ -59,34 +69,65 @@ contract Players is ERC721, ERC721Burnable, Ownable {
         return player.fishingLevel;
     }
 
-    function setXp(uint256 _playerId, string memory _skill, uint256 _xpGained) internal {
+    // function setXp(uint256 _playerId, string memory _skill, uint256 _xpGained) internal {
+    //     Player storage player = players[_playerId];
+    //         player.currentFishingXp += _xpGained;
+    // }
+
+    function levelUp(uint256 _playerId) internal {
         Player storage player = players[_playerId];
-        if (keccak256(bytes(_skill)) == keccak256(bytes("fishing"))) {
-            player.currentFishingXp += _xpGained;
-        }
+        player.fishingLevel += 1;
+        player.currentFishingXp = player.currentFishingXp - player.fishingXpForLevel;
+        player.fishingXpForLevel = uint256(player.fishingLevel) * 100;
     }
 
-    function levelUp(uint256 _playerId, string memory _skill) internal {
-        Player storage player = players[_playerId];
-        if (keccak256(bytes(_skill)) == keccak256(bytes("fishing"))) {
-            player.fishingLevel += 1;
-            player.currentFishingXp = currentFishingXp - fishingXpForLevel;
-            player.fishingXpForLevel = uint256(player.fishingLevel) * 100;
-        }
+    // function quest(uint256 _playerId, string memory _skill, string memory _type) public onlyOwnerOf(_playerId) {
+    //     Player storage player = players[_playerId];
+    //     if (player.currentFishingXp >= player.fishingXpForLevel) {
+    //         levelUp(_playerId, _skill);
+    //     }
+
+    //     if (keccak256(bytes(_skill)) == keccak256(bytes("fishing"))) {
+    //         if (keccak256(bytes(_skill)) == keccak256(bytes("shrimp"))) {
+    //             items.mint(msg.sender, 11, 1, "");
+    //             setXp(_playerId, _skill, 25);
+    //         }
+    //     }
+    // }
+
+    function quest(uint256 _playerId, uint256 _questType) public onlyOwnerOf(_playerId){
+       // Player storage player = players[_playerId];
+        safeTransferFrom(msg.sender, address(this), _playerId);
+        playerQuesting[_playerId].isQuesting = true;
+        playerQuesting[_playerId].typeQuest = _questType;
+        playerQuesting[_playerId].time = block.timestamp;
+        playerQuesting[_playerId].owner = msg.sender;
     }
 
-    function quest(uint256 _playerId, string memory _skill, string memory _type) public onlyOwnerOf(_playerId) {
+    function withdraw(uint256 _playerId) public {
+        require(
+            playerQuesting[_playerId].owner == msg.sender, "Not owner"
+        );
+        require(
+            playerQuesting[_playerId].isQuesting, "Not Questing"
+        );
+        rewards(_playerId);
+        approve(msg.sender, _playerId);
+        safeTransferFrom(address(this), msg.sender, _playerId);
+        
+        playerQuesting[_playerId].isQuesting = false;
+        playerQuesting[_playerId].typeQuest = 0;
+        playerQuesting[_playerId].time = block.timestamp;
+    }
+
+    function rewards(uint256 _playerId) internal {
         Player storage player = players[_playerId];
+        uint256 timeDifference = block.timestamp - playerQuesting[_playerId].time;
+        items.mint(msg.sender, 11, timeDifference, "");
         if (player.currentFishingXp >= player.fishingXpForLevel) {
-            levelUp(_playerId, _skill);
+            levelUp(_playerId);
         }
-
-        if (keccak256(bytes(_skill)) == keccak256(bytes("fishing"))) {
-            if (keccak256(bytes(_skill)) == keccak256(bytes("shrimp"))) {
-                items.mint(msg.sender, 11, 1, "");
-                setXp(_playerId, _skill, 25);
-            }
-        }
+        player.currentFishingXp += timeDifference;
     }
 
     function _baseURI() internal pure override returns (string memory) {
